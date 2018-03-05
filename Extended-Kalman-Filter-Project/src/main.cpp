ff441cf9c138e2e1d0a430b7ae405d273d3f7e2d
Wubai Zhou <wubai.zhou@uber.com>
2018-03-04 19:47:29 -0800

    add jupyter for c++ and some comments for c++ lambda

--- Extended-Kalman-Filter-Project/src/main.cpp
+++ Extended-Kalman-Filter-Project/src/main.cpp
@@ -38,6 +38,8 @@ int main()
   vector<VectorXd> estimations;
   vector<VectorXd> ground_truth;
 
+  //C++ callback function that defines as anonymous lambda function https://msdn.microsoft.com/en-us/library/dd293608.aspx
+  // [&] means all variables that you refer to are captured by reference, and [=] means they are captured by value
   h.onMessage([&fusionEKF,&tools,&estimations,&ground_truth](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
