91346baf55df03b4d38a68ed82e93140faacf47f
Wubai Zhou <wubai.zhou@uber.com>
2018-03-08 23:07:47 -0800

    add predict mean and covariance in ukf

--- Unscented-Kalman-Filter-Project/src/ukf.cpp
+++ Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -174,9 +174,10 @@ void UKF::AugmentedSigmaPoints(MatrixXd *Xsig_out){
 
 
 /**
-*
+* Predict augmented sigma points to state sigma points
+* @Param {MatrixXd*} Xsig_out
 */
-void SigmaPointPrediction(MatrixXd* Xsig_out){
+void UKF::SigmaPointPrediction(MatrixXd* Xsig_out){
   //set state dimension
   int n_x = 5;
 
@@ -217,7 +218,7 @@ void SigmaPointPrediction(MatrixXd* Xsig_out){
     VectorXd delta_x(n_x);
     VectorXd noise_x(n_x);
     // if yaw rate is 0, car is going straight
-    if (rho_dot == 0){
+    if (fabs(rho_dot) < 0.001){
         delta_x << v * c_rho * delta_t, v * s_rho * delta_t, 0, 0, 0;
     } else {
         float vr_dot = v / rho_dot;
@@ -230,4 +231,67 @@ void SigmaPointPrediction(MatrixXd* Xsig_out){
 
   *Xsig_out = Xsig_pred;
 }
+
+/**
+* Given predicted state sigma points, estimate its mean and covariances
+* @Param {VectorXd*} x_pred
+* @Param {Matrix*} P_pred
+*/
+void UKF::PredictMeanAndCovariance(VectorXd* x_out, MatrixXd* P_out){
+  //set state dimension
+  int n_x = 5;
+
+  //set augmented dimension
+  int n_aug = 7;
+
+  //define spreading parameter
+  double lambda = 3 - n_aug;
+
+  //create example matrix with predicted sigma points
+  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+  Xsig_pred <<
+         5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
+           1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
+          2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
+         0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
+          0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
+
+  //create vector for weights
+  VectorXd weights = VectorXd(2*n_aug+1);
+  
+  //create vector for predicted state
+  VectorXd x = VectorXd(n_x);
+
+  //create covariance matrix for prediction
+  MatrixXd P = MatrixXd(n_x, n_x);
+
+  //set weights
+  //predict state mean
+  //predict state covariance matrix
+  weights(0) = lambda / (lambda + n_aug);
+  for (int i = 0; i < 2 * n_aug + 1; i++){
+    weights(i) = 0.5 / (lambda + n_aug);
+  }
+  
+  x.fill(0.0);
+  for (int i = 0; i < 2 * n_aug + 1; i++){
+    x += weights(i) * Xsig_pred.col(i);
+  }
+  
+  P.fill(0.0);
+  for(int i = 0; i < 2 * n_aug + 1; i++){
+    VectorXd residual = Xsig_pred.col(i) - x;
+    P += weights(i) * (residual * residual.transpose());
+  }
+  
+  //write result
+  *x_out = x;
+  *P_out = P;
+}
+
+
+
+
+
+
 
--- Unscented-Kalman-Filter-Project/src/ukf.h
+++ Unscented-Kalman-Filter-Project/src/ukf.h
@@ -106,6 +106,9 @@ public:
   void GenerateSigmaPoints(MatrixXd* Xsig_out);
   void AugmentedSigmaPoints(MatrixXd* Xsig_out);
   void SigmaPointPrediction(MatrixXd* Xsig_out);
+  void PredictMeanAndCovariance(VectorXd* x_pred, MatrixXd* P_pred);
+  void PredictRadarMeasurement(VectorXd* z_out, MatrixXd* S_out);
+  void UpdateState(VectorXd* x_out, MatrixXd* P_out);
 };
 
 #endif /* UKF_H */
