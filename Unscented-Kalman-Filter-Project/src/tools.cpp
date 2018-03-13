85fcae44fa18f2bd6af54635f12d37340c1103d9
Wubai Zhou <wubai.zhou@uber.com>
2018-03-12 21:23:01 -0700

    add calculate mrse

--- Unscented-Kalman-Filter-Project/src/tools.cpp
+++ Unscented-Kalman-Filter-Project/src/tools.cpp
@@ -15,4 +15,25 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    
+    if (estimations.size() != ground_truth.size()){
+        cout << "CalculationRMSE error: dismatch size" <<endl;
+        return rmse;
+    } else if (estimations.size() == 0){
+        cout << "CalculationRMSE error: size 0" <<endl;
+        return rmse;
+    }
+    
+    //accumulate squared residuals
+    for(int i = 0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
+        rmse += VectorXd(residual.array() * residual.array());
+    }
+    
+    rmse = rmse / estimations.size();
+    rmse = rmse.array().sqrt();
+    
+    return rmse;
 }
