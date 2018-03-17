f01f4fe3c8cfe79450b70d1e3813ab73e5207a51
Wubai Zhou <wubai.zhou@uber.com>
2018-03-16 23:00:08 -0700

    finish init method for particles

--- Kidnapped-Vehicle-Project/src/particle_filter.cpp
+++ Kidnapped-Vehicle-Project/src/particle_filter.cpp
@@ -24,7 +24,24 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-
+    num_particles = 50;
+    
+    default_random_engine gen;
+    normal_distribution<double> dist_x(x, std[0]);
+    normal_distribution<double> dist_y(y, std[1]);
+    normal_distribution<double> dist_theta(theta, std[2]);
+    
+    for (int i = 0; i < num_particles; i++){
+        Particle p = {
+            i, // set id to i ?
+            dist_x(gen),
+            dist_y(gen),
+            dist_theta(gen),
+            1.0 / num_particles // inital weight
+        };
+        particles.push_back(p);
+    }
+    is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
