2c12fcf9f2373928994cb2ea56256f374bb74d7c
Wubai Zhou <wubai.zhou@uber.com>
2018-03-22 16:49:16 -0700

    rename member variable

--- PID-Control-Project/src/PID.cpp
+++ PID-Control-Project/src/PID.cpp
@@ -11,11 +11,16 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    Kp_ = Kp;
+    Ki_ = Ki;
+    Kd_ = Kd;
 }
 
 void PID::UpdateError(double cte) {
+
 }
 
 double PID::TotalError() {
+
 }
 
