7b2efcee93e2eae26ff27fde8a832989b8a5a00a
Wubai Zhou <wubai.zhou@uber.com>
2018-03-22 20:58:22 -0700

    finish the PID for steer value

--- PID-Control-Project/src/PID.cpp
+++ PID-Control-Project/src/PID.cpp
@@ -14,13 +14,20 @@ void PID::Init(double Kp, double Ki, double Kd) {
     Kp_ = Kp;
     Ki_ = Ki;
     Kd_ = Kd;
+    
+    p_error_ = 0.0;
+    d_error_ = 0.0;
+    i_error_ = 0.0;
 }
 
 void PID::UpdateError(double cte) {
-
+    p_error_ = cte;
+    d_error_ = cte - d_error_;
+    i_error_ += cte;
 }
 
 double PID::TotalError() {
-
+    double error = - Kp_ * p_error_ - Kd_ * d_error_ - Ki_ * i_error_;
+    return error;
 }
 
