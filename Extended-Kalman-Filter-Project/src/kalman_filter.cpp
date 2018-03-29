b2af663d7da9aff56daffcfedcbba87d77fa93c8
Wubai Zhou <wubai.zhou@uber.com>
2018-03-29 12:39:23 -0700

    add notebook change

--- Extended-Kalman-Filter-Project/src/kalman_filter.cpp
+++ Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -4,7 +4,7 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -41,7 +41,7 @@ void KalmanFilter::Update(const VectorXd &z) {
     MatrixXd S = H_ * P_ * H_t + R_;
     MatrixXd S_inv = S.inverse();
     MatrixXd K = P_ * H_t * S_inv;
-    
+
     //new estimate
     x_ = x_ + K * y;
     P_ = P_ - K * H_ * P_;
@@ -53,7 +53,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     * update the state by using Extended Kalman Filter equations
     * it is exactly the same as Update, only z, H_, R_ is different (size,)
   */
-  
+
   // to calculate z, we will use the non-linear h(x) function directly
   VectorXd z_pred(3);
   float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
@@ -63,15 +63,15 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   float phi = atan2(x_(1), x_(0)); // normalize phi
   float rho_dot = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
   z_pred << rho, phi, rho_dot;
-  
+
   VectorXd y = z - z_pred;
   y(1) = atan2(sin(y(1)), cos(y(1))); // normalize phi after substraction
-  
+
   MatrixXd H_t = H_.transpose();
   MatrixXd S = H_ * P_ * H_t + R_;
   MatrixXd S_inv = S.inverse();
   MatrixXd K = P_ * H_t * S_inv;
-    
+
   //new estimate
   x_ = x_ + K * y;
   P_ = P_ - K * H_ * P_;
